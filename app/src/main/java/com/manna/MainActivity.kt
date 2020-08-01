2c729353e1f9a53c0f40722c763b258b891f4ee0
LEEYEONJAE <eunoiaa22@gmail.com>
2020-08-01 21:40:35 +0900

    style(MainActivity) : MainActivity import 정리

--- app/src/main/java/com/manna/MainActivity.kt
+++ app/src/main/java/com/manna/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.manna
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(R.id.frame, FriendRequestFragment.newInstance())
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame, FriendRequestFragment.newInstance())
             .commit()
     }
 }
